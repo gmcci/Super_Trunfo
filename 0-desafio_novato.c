e64cf26f935ff4f9b4348dd5bea673897910c30b
gmcci <guilherme.maracci@gmail.com>
2026-01-14 21:46:20 -0300

    Atualizado 14 de janeiro de 2026

---
+++ 0-desafio_novato.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+
+int main(){
+
+    // Variáveis da primeira cidade
+    char estado0;
+    char codigo0;
+    char cidade0[20];
+    int populacao0 = 0;
+    float area0 = 0;
+    float pib0 = 0;
+    int pontos0 = 0;
+
+    // Variáveis da segunda cidade
+    char estado1;
+    char codigo1;
+    char cidade1[20];
+    int populacao1 = 0;
+    float area1 = 0;
+    float pib1 = 0;
+    int pontos1 = 0;
+
+
+    // Cidade 1
+
+    printf("\nCarta 1:\n");   
+    printf("\nEstado [A-H]: ");
+    scanf(" %c", &estado0);
+
+    printf("Código [1-4]: ");
+    scanf(" %c", &codigo0);
+
+    /* Para não receber o ENTER da
+       variável anterior e pular
+       a resposta de cidade */
+
+    while(getchar() != '\n');
+
+    printf("Cidade: ");
+    fgets(cidade0, sizeof(cidade0), stdin);
+
+    printf("População: ");
+    scanf("%d", &populacao0);
+
+    printf("Área in km²: ");
+    scanf("%f", &area0);
+
+    printf("PIB: ");
+    scanf("%f", &pib0);
+
+    printf("Pontos turísticos: ");
+    scanf("%d", &pontos0);
+
+
+    // Cidade 2
+
+    printf("\n\nCarta 2: \n");
+    printf("\nEstado [A-H]: ");
+    scanf(" %c", &estado1);
+
+    printf("Código [1-4]: ");
+    scanf(" %c", &codigo1);
+
+    /* Para não receber o ENTER da
+       variável anterior e pular
+       a resposta de cidade */
+
+    while(getchar() != '\n');
+
+    printf("Cidade: ");
+    fgets(cidade1, sizeof(cidade1), stdin);
+
+    printf("População: ");
+    scanf("%d", &populacao1);
+
+    printf("Área in km²: ");
+    scanf("%f", &area1);
+
+    printf("PIB: ");
+    scanf("%f", &pib1);
+
+    printf("Pontos turísticos: ");
+    scanf("%d", &pontos1);
+
+
+    // Print geral
+ 
+    printf("\n\n");
+    printf("----------------------------------------------------");
+    printf("\nCarta 1: \n\n");
+    printf("Estado: %c\n", estado0);
+    printf("Código: %c0%c\n", estado0, codigo0);
+    printf("Cidade: %s", cidade0);
+    printf("População: %d\n", populacao0);
+    printf("Área: %.2f km²\n", area0);
+    printf("PIB: %.2f bilhões de reais\n", pib0);
+    printf("Pontos turísticos: %d\n", pontos0);
+    printf("\n");
+
+    printf("\nCarta 2: \n\n");
+    printf("Estado: %c\n", estado1);
+    printf("Código: %c0%c\n", estado1, codigo1);
+    printf("Cidade: %s", cidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Pontos turísticos: %d\n", pontos1);
+    printf("\n");
+    printf("----------------------------------------------------\n\n");
+
+    return 0;
+}
